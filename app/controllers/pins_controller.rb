ec4b8a20855673d7b64b7a98b83497ec0397d91a
Jordan Randolph <jwrandolph97@gmail.com>
2016-03-19 19:06:40 -0400

    User Security and interface. Problem: Editing others' posts can still be done.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,7 @@
+
 class PinsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -11,14 +14,14 @@ class PinsController < ApplicationController
 
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
     if @pin.save
      redirect_to @pin, notice: 'Pin was successfully created.' 
@@ -48,6 +51,10 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+    def correct_user
+        @pin = current_user.pins.find_by(params[:id])
+        redirect_to pins_path, notice: "Not authorized to edit this pin!" if @pin.nil?
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
