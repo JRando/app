2b2fd53012149d6a8f6120d6d36eceea40b2041c
Jordan Randolph <jwrandolph97@gmail.com>
2016-03-31 18:34:54 -0400

    troubleshooting

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,17 +1,14 @@
-
 class PinsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 6)
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
   end
-
 
   def show
   end
-
 
   def new
     @pin = current_user.pins.build
@@ -22,27 +19,24 @@ class PinsController < ApplicationController
 
   def create
     @pin = current_user.pins.build(pin_params)
-
     if @pin.save
-     redirect_to @pin, notice: 'Pin was successfully created.' 
+      redirect_to @pin, notice: 'Wow... you really pinned me ;)'
     else
-     render :new 
+      render action: 'new'
     end
   end
 
   def update
-      if @pin.update(pin_params)
-         redirect_to @pin, notice: 'Pin was successfully updated.' 
-      else
-         render :edit 
-
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'OH YEAH!!!!'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
     @pin.destroy
-    redirect_to pins_url, notice: 'Pin was successfully destroyed.'
-      
+    redirect_to pins_url
   end
 
   private
@@ -52,13 +46,12 @@ class PinsController < ApplicationController
     end
 
     def correct_user
-        @pin = current_user.pins.where(params[:id])
-        redirect_to root, notice: "Not authorized to edit this pin!" if @pin.nil?
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "You can't do that..." if @pin.nil?
     end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description, :image)
     end
 end
-
-
